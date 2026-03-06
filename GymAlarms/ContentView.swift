16867f13b4e88705cb19de2c07b299a6be882503
YairAndrade <y.andrade1@uniandes.edu.co>
2026-03-06 13:47:06 -0500

    Initial Commit

---
+++ GymAlarms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GymAlarms
+//
+//  Created by Estudiantes on 6/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GymAlarms/GymAlarmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAlarmsApp.swift
+//  GymAlarms
+//
+//  Created by Estudiantes on 6/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GymAlarmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
